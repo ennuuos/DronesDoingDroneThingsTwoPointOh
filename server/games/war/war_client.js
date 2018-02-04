017e195a9fa646b03c8113be3b78e0b1fbb234b9
Travis Valenti <ennuuos@gmail.com>
2018-02-05 10:56:27 +1100

    Add war game front-end

--- server/games/war/war_client.js
+++ server/games/war/war_client.js
@@ -4,12 +4,32 @@ const game_module = () => {
 	console.log("Loaded war game");
 
 	let game_div = document.getElementById('game_div');
+    game_div.style = "display:grid; grid-template-columns: 50% 50%; text-align: center; vertical-align:middle; font-size: 120px;";
 
-	let canvas = document.createElement('canvas')
-	game_div.appendChild(canvas);
-	let context = canvas.getContext('2d');
+    let score_one = 1;
+    let score_two = 3;
+
+	let score_one_div = document.createElement('div')
+    game_div.appendChild(score_one_div);
+    let score_two_div = document.createElement('div')
+    game_div.appendChild(score_two_div);
+
+    score_one_div.style = "border-right: 2px solid grey;"
+    score_two_div.style = "border-left: 2px solid grey;"
+
+	setInterval(() => {
+        score_one_div.innerHTML = score_one;
+        score_two_div.innerHTML = score_two;
+    }, 100);
 
 
-	//setInterval(()=>{console.log(drone_data)}, 100);
+};
 
-	};
+const fetchScores = function() {
+    fetch('/game/scores')
+    .then(res => res.json())
+    .then(data => {
+        score_one = data.score_one;
+        score_two = data.score_two;
+    }).catch(err => console.log(err));
+}
