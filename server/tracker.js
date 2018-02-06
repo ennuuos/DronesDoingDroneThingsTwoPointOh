57494769c1119fd6dcfe7713d72afed7407b3ead
tomw <aeduscreative@gmail.com>
2018-02-06 15:47:59 +1100

    savePng function added

--- server/tracker.js
+++ server/tracker.js
@@ -4,7 +4,6 @@ makeCamera = function(id) {
   console.log('Cameras for drone ' + id + ' created');
 }
 
-
 getPng = function(id) {
   console.log('getting Png');
 
@@ -15,7 +14,44 @@ getPng = function(id) {
       });
      }
 
+savePng = function(path, picture) {
+  var fs = require('fs');
+  console.log('Saving Image');
+  fs.writeFile(path, picture, function(err) {
+    if (err) {
+      console.log('Something fucked up: ' + err);
+    };
+  });
+}
 
+/*begin insert
+var fs = require('fs');
+var pngStream = drones[1].getPngStream();
+var frameCounter = 0;
+var saveDir = '/home/Projects/DronesDoingDroneThingsTwoPointOh/Images';
+
+console.log('Inside PNGStream')
+
+pngStream
+  .on('error', console.log)
+  .on('data', function(pngBuffer) {
+
+
+  var imageName = saveDir + '/savePNG' + frameCounter +
+'.png';
+  fs. writeFile(imageName, pngBuffer, function(err) {
+    if (err) {
+      console.log('Error saving PNG: ' + err);
+    }
+  });
+
+  console.log(imageName);
+
+  frameCounter++;
+});
+
+console.log('Leaving PNGStream');
+end insert */
 
 module.exports = {
   makeCamera: makeCamera,
