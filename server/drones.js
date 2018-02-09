5fcd7c8cc516ad67fef02a75529334e016f84438
tomw <aeduscreative@gmail.com>
2018-02-09 14:52:15 +1100

    neater camera code

--- server/drones.js
+++ server/drones.js
@@ -37,10 +37,10 @@ for(let i = 0; i < 10; i++) {
 				if(!navdata[i]) console.log(`Drone ${i} connected`);
 				navdata[i] = data.demo;
 			}
-            pngStreams[i] = drones[i].getPngStream()
-                //.on('error', console.log)
+            drones[i].getPngStream()
+                .on('error', console.log)
                 .on('data', pngBuffer => {images[i] = pngBuffer})
-            videoStreams[i] = drones[i].getVideoStream();
+            drones[i].getVideoStream();
 		});
 }
 
@@ -56,9 +56,6 @@ const remove = (id) => {
     console.log(`Drone ${id} disconnected`);
     delete list[id];
 	  if(navdata[id]) delete navdata[id];
-    delete pngStreams[id];
-    delete videoStreams[id];
-
 };
 
 const control = (id, action, degree) => {
